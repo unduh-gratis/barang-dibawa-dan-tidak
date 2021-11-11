8f5f0c6975ca802c212a6ce195c936ffb7ab5fab
unduh-gratis <penghapuspulpen@gmail.com>
2021-11-11 13:35:50 +0700

    feat: Refactor

---
+++ app/Http/Controllers/RequiredProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RequiredProduct;
+use Illuminate\Http\Request;
+
+class RequiredProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RequiredProduct  $requiredProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RequiredProduct $requiredProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RequiredProduct  $requiredProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RequiredProduct $requiredProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RequiredProduct  $requiredProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RequiredProduct $requiredProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RequiredProduct  $requiredProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RequiredProduct $requiredProduct)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\RequiredProduct;
+use App\Models\User;
+use App\Models\UserProduct;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,15 +19,21 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
         \App\Models\Product::factory(100)->create();
 
-        $users = \App\Models\User::all();
+        $users = User::all();
         foreach ($users as $user) {
-            for ($i=0; $i < 10; $i++) {
-                \App\Models\UserProduct::create([
+            for ($i=1; $i <= 5; $i++) {
+                UserProduct::create([
                     'user_id' => $user->id,
-                    'product_id' => random_int(1, 100),
+                    'product_id' => $i,
                     'tanggal' => now()
                 ]);
             }
+        }
+
+        for ($i=1; $i <= 10; $i++) {
+            RequiredProduct::create([
+                'product_id' => $i
+            ]);
         }
     }
 }
